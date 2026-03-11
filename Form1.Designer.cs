8c4bf7f106ac729a056737416c6c3a5728c52e63
Rift <61116182+Riftaricus@users.noreply.github.com>
2026-03-11 11:23:19 +0100

    Add menu strip UI and resource support to main form
    Added a MenuStrip to Form1 with "Game", "Actions", and "Dealer" menus, each containing relevant subitems. Stub event handlers for menu actions were created. Updated resource files (Form1.resx, Resources.resx, Resources.Designer.cs) and project file to support resource management and code generation. These changes establish the foundation for a menu-driven interface.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            menuStrip1 = new MenuStrip();
+            testToolStripMenuItem = new ToolStripMenuItem();
+            testToolStripMenuItem1 = new ToolStripMenuItem();
+            testToolStripMenuItem2 = new ToolStripMenuItem();
+            actionsToolStripMenuItem = new ToolStripMenuItem();
+            standToolStripMenuItem = new ToolStripMenuItem();
+            hitToolStripMenuItem = new ToolStripMenuItem();
+            dealerToolStripMenuItem = new ToolStripMenuItem();
+            shuffleDeckToolStripMenuItem = new ToolStripMenuItem();
+            dealCradsToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Dock = DockStyle.Bottom;
+            menuStrip1.GripStyle = ToolStripGripStyle.Visible;
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { testToolStripMenuItem, actionsToolStripMenuItem, dealerToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 422);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.RenderMode = ToolStripRenderMode.System;
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // testToolStripMenuItem
+            // 
+            testToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { testToolStripMenuItem1, testToolStripMenuItem2 });
+            testToolStripMenuItem.Name = "testToolStripMenuItem";
+            testToolStripMenuItem.Size = new Size(62, 24);
+            testToolStripMenuItem.Text = "Game";
+            testToolStripMenuItem.TextDirection = ToolStripTextDirection.Horizontal;
+            testToolStripMenuItem.Click += testToolStripMenuItem_Click;
+            // 
+            // testToolStripMenuItem1
+            // 
+            testToolStripMenuItem1.Name = "testToolStripMenuItem1";
+            testToolStripMenuItem1.Size = new Size(224, 26);
+            testToolStripMenuItem1.Text = "Start Game";
+            testToolStripMenuItem1.Click += testToolStripMenuItem1_Click;
+            // 
+            // testToolStripMenuItem2
+            // 
+            testToolStripMenuItem2.Name = "testToolStripMenuItem2";
+            testToolStripMenuItem2.Size = new Size(224, 26);
+            testToolStripMenuItem2.Text = "Settings";
+            // 
+            // actionsToolStripMenuItem
+            // 
+            actionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { standToolStripMenuItem, hitToolStripMenuItem });
+            actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            actionsToolStripMenuItem.Size = new Size(72, 24);
+            actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // standToolStripMenuItem
+            // 
+            standToolStripMenuItem.Name = "standToolStripMenuItem";
+            standToolStripMenuItem.Size = new Size(224, 26);
+            standToolStripMenuItem.Text = "Stand";
+            // 
+            // hitToolStripMenuItem
+            // 
+            hitToolStripMenuItem.Name = "hitToolStripMenuItem";
+            hitToolStripMenuItem.Size = new Size(224, 26);
+            hitToolStripMenuItem.Text = "Hit";
+            // 
+            // dealerToolStripMenuItem
+            // 
+            dealerToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { shuffleDeckToolStripMenuItem, dealCradsToolStripMenuItem });
+            dealerToolStripMenuItem.Name = "dealerToolStripMenuItem";
+            dealerToolStripMenuItem.Size = new Size(67, 24);
+            dealerToolStripMenuItem.Text = "Dealer";
+            dealerToolStripMenuItem.Click += dealerToolStripMenuItem_Click;
+            // 
+            // shuffleDeckToolStripMenuItem
+            // 
+            shuffleDeckToolStripMenuItem.Name = "shuffleDeckToolStripMenuItem";
+            shuffleDeckToolStripMenuItem.Size = new Size(175, 26);
+            shuffleDeckToolStripMenuItem.Text = "Shuffle Deck";
+            // 
+            // dealCradsToolStripMenuItem
+            // 
+            dealCradsToolStripMenuItem.Name = "dealCradsToolStripMenuItem";
+            dealCradsToolStripMenuItem.Size = new Size(175, 26);
+            dealCradsToolStripMenuItem.Text = "Deal Cards";
+            dealCradsToolStripMenuItem.Click += dealCradsToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
             Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem testToolStripMenuItem;
+        private ToolStripMenuItem testToolStripMenuItem1;
+        private ToolStripMenuItem testToolStripMenuItem2;
+        private ToolStripMenuItem actionsToolStripMenuItem;
+        private ToolStripMenuItem standToolStripMenuItem;
+        private ToolStripMenuItem hitToolStripMenuItem;
+        private ToolStripMenuItem dealerToolStripMenuItem;
+        private ToolStripMenuItem dealCradsToolStripMenuItem;
+        private ToolStripMenuItem shuffleDeckToolStripMenuItem;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Blackjack_Dealer_Training
 {
     public partial class Form1 : Form
@@ -5,6 +7,26 @@ namespace Blackjack_Dealer_Training
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void testToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dealerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dealCradsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
